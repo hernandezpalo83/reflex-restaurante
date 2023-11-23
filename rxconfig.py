a2fd2705b3823e69d6086aa6f5b323cbf13fbc4c
Javi <hernandezpalo@gmail.com>
2023-11-23 21:46:32 +0100

    .

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="restaurante",
-    api_url="https://rxh-prod-restaurante.fly.dev",
+    api_url="https://rxh-prod-restaurante.fly.dev:8000",
 )
